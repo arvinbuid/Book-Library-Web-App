20c8b7a5bb102ea9df7f59ce6735561fea622db6
Arvin Buid <arvinbuid01@gmail.com>
2024-06-02 18:51:39 +0800

    Initialized project starting files.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <p className='3xl font-bold text-center'>Book Library App</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
